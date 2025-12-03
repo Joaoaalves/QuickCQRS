a3501b19325429e925fd2b19a7b62f9cc37fa2e6
Joaaalves <contato@joao-alves.com>
2025-12-03 14:53:54 -0300

    Feat: add modules

---
+++ src/Joaoaalves.FastCQRS.Infrastructure/Processing/MediatorModule.cs
@@ -0,0 +1,93 @@
+using System.Reflection;
+using Joaoaalves.FastCQRS.Application.Execution;
+using Joaoaalves.FastCQRS.Application.Validation;
+using Joaoaalves.FastCQRS.Domain.Notifications;
+using Joaoaalves.FastCQRS.Domain.Requests;
+using Joaoaalves.FastCQRS.Infrastructure.Persistence;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Joaoaalves.FastCQRS.Infrastructure.Processing
+{
+    /// <summary>
+    /// Provides methods to configure the mediator module, including command/query handlers and pipeline behaviors.
+    /// </summary>
+    public static class MediatorModule
+    {
+        /// <summary>
+        /// Registers MediatR-like services and command/query processing pipeline.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        /// <param name="args">Assemblies or assembly name prefixes to scan for handlers.</param>
+        /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddMediatorModule(this IServiceCollection services, params object[] args)
+        {
+            services.AddScoped<IMediator, Mediator>();
+            services.AddScoped<IDomainEventsDispatcher, DomainEventsDispatcher>();
+
+            var assemblies = ResolveAssemblies(args);
+            RegisterHandlers(services, assemblies, typeof(INotificationHandler<>));
+            RegisterHandlers(services, assemblies, typeof(IRequestHandler<,>));
+
+            services.AddScoped<CommandsExecutor>();
+            services.AddScoped<QueriesExecutor>();
+
+            services.AddScoped(typeof(IRequestPipelineBehavior<,>), typeof(CommandValidationBehavior<,>));
+            services.AddScoped(typeof(IRequestPipelineBehavior<,>), typeof(UnitOfWorkPipelineBehavior<,>));
+
+            return services;
+        }
+
+        /// <summary>
+        /// Resolves assemblies from input parameters to be scanned for handler types.
+        /// </summary>
+        /// <param name="args">Can be an array of assemblies or string prefixes.</param>
+        /// <returns>An array of resolved assemblies.</returns>
+        public static Assembly[] ResolveAssemblies(object[] args)
+        {
+            if (args == null || args.Length == 0)
+                return AppDomain.CurrentDomain
+                    .GetAssemblies()
+                    .Where(a => !a.IsDynamic && !string.IsNullOrWhiteSpace(a.FullName))
+                    .ToArray();
+
+            if (args.All(a => a is Assembly))
+                return args.Cast<Assembly>().ToArray();
+
+            if (args.All(a => a is string))
+            {
+                var prefixes = args.Cast<string>().ToArray();
+
+                return AppDomain.CurrentDomain
+                    .GetAssemblies()
+                    .Where(a =>
+                        !a.IsDynamic &&
+                        !string.IsNullOrWhiteSpace(a.FullName) &&
+                        prefixes.Any(p => a.FullName!.StartsWith(p))
+                    ).ToArray();
+            }
+
+            throw new ArgumentException("Invalid Parameters for AddMediatorModule.");
+        }
+
+        /// <summary>
+        /// Registers all types implementing a specific handler interface (e.g., IRequestHandler, INotificationHandler).
+        /// </summary>
+        public static void RegisterHandlers(IServiceCollection services, Assembly[] assemblies, Type handlerInterface)
+        {
+            var types = assemblies.SelectMany(a => a.GetTypes())
+                .Where(t => t.IsClass && !t.IsAbstract)
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var interfaces = type.GetInterfaces()
+                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == handlerInterface);
+
+                foreach (var intrfc in interfaces)
+                {
+                    services.AddTransient(intrfc, type);
+                }
+            }
+        }
+    }
+}
