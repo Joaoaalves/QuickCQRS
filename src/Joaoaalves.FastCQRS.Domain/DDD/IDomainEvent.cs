8dc99ffad17f8c2882755adb85b97b7e6e9dee54
Joaaalves <contato@joao-alves.com>
2025-12-03 14:24:52 -0300

    feat: added DDD contracts

---
+++ src/Joaoaalves.FastCQRS.Domain/DDD/IDomainEvent.cs
@@ -0,0 +1,16 @@
+using Joaoaalves.FastCQRS.Domain.Notifications;
+
+namespace Joaoaalves.FastCQRS.Domain.DDD
+{
+    /// <summary>
+    /// Represents a domain event that indicates something important has happened within the domain.
+    /// Domain events are used to notify other parts of the system about changes in state.
+    /// </summary>
+    public interface IDomainEvent : INotification
+    {
+        /// <summary>
+        /// Gets the timestamp indicating when the domain event occurred.
+        /// </summary>
+        DateTime OccurredOn { get; }
+    }
+}
